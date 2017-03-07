be68038cda29549fa4c8e9d2dddad4db04812122
Pé Đào SNSD <P o SNSD>
2017-03-07 20:26:40 +0700

    file connection

---
+++ connect.php
@@ -0,0 +1,19 @@
+<?php
+
+	function Connection(){
+		$localhost="localhost";
+		$user="root";
+		$pass="";
+		$db="iot";
+	   	
+		$connection = mysqli_connect($localhost, $user, $pass);
+
+		if (!$connection) {
+	    	die('MySQL ERROR: ' . mysqli_error());
+		}
+		
+		mysqli_select_db($connection,$db) or die( 'MySQL ERROR: '. mysqli_error($connection) );
+
+		return $connection;
+	}
+?>
