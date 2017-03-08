2f8c99d7b44e4c28ad63fcfc985d8be2a1708213
Pé Đào SNSD <P o SNSD>
2017-03-08 14:00:20 +0700

    login

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+   include("connect.php");
+   $link=Connection();
+   session_start();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+   
+      $myemail = mysqli_real_escape_string($link,$_POST['email']);
+      $mypassword = mysqli_real_escape_string($link,$_POST['password']); 
+      
+      $sql = "SELECT * FROM users WHERE email = '$myemail' and password = '$mypassword'";
+      $result = mysqli_query($link,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);    
+      $count = mysqli_num_rows($result);
+		
+      if($count == 1) {     
+         header("location: data.php");
+      }else {
+      	echo "<script>
+			alert('Your Login Name or Password is invalid');
+			window.location.href='index.html';
+			</script>";
+      }
+   }
+?>
