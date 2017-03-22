da703cbefcb6ba3e878f297d5f55516e14fb0b71
Xuân Đỗ <Xun >
2017-03-22 12:27:43 +0700

    change layout

--- showData.php
+++ showData.php
@@ -55,7 +55,6 @@
             <!-- sidebar menu -->
             <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
               <div class="menu_section">
-                <h3>General</h3>
                 <ul class="nav side-menu">
                   <li><a href="index.php"><i class="fa fa-home"></i> Home </a></li>
                   <li><a href="showData.php"><i class="fa fa-table"></i>Data Table</a></li>               
