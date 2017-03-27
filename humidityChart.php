cbe6b88962eb4a6552e57dbf3b9cff7162397e34
Xuân Đỗ <Xun >
2017-03-27 16:00:22 +0700

    change title

--- humidityChart.php
+++ humidityChart.php
@@ -103,7 +103,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Temperature page</title>
+    <title>Humidity page</title>
 
     <link href="vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
